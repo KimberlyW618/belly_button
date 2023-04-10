e744e6e0ff6005be3c4f920760a69cb7c8a5d156
KimberlyW618 <kimw618@gmail.com>
2023-04-09 22:16:57 -0700

    commit5

--- charts.js
+++ charts.js
@@ -12,62 +12,9 @@
 // open with live server
 // it will provide a link, prob >
 // http://localhost:5500/13-BellyButton/belly_button/
-// to end session press command c? 
+// to end session press 
 
 ////////
-/*
-// create a dynamic dropdown menu of ID numbers 
-function init() {
-  var selector = d3.select("#selDataset");
-
-  d3.json("samples.json").then((data) => {
-    console.log(data);
-    var sampleNames = data.names;
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-})}
-
-init();
-
-// NOTE: this of this.value from the html and newSample from js are = to eachother and refer to the dropdown menu
-// When a change takes place to the dropdown menu, call functions buildMetadata & buildCharts
-function optionChanged(newSample) {
-  console.log(newSample);
-}
-function optionChanged(newSample) {
-  buildMetadata(newSample);
-  buildCharts(newSample);
-}
-
-// Demographics Panel 
-function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
-    var metadata = data.metadata;
-    // Filter the data for the object with the desired sample number
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-   
-    // Use d3 to select the panel with id of `#sample-metadata`
-    var PANEL = d3.select("#sample-metadata");
-
-    // Use `.html("") to clear any existing metadata
-    PANEL.html("");
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-    });
-
-  });
-}
-*/
-
-
-
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
